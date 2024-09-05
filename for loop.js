7d141ef37e0defa032c280571c93e9f3b3d018b1
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 14:48:41 +0600

    even off by for loop

--- for loop.js
+++ for loop.js
@@ -3,3 +3,13 @@
 for (let num = 1; num <= 5 ; num++){
     console.log(num)
 }
+
+// even numbers print using for loop 
+for (let i = 0; i<=20; i=i+2){
+    console.log('even number:', i)
+}
+
+// odd number print using for loop 
+for (let i = 1; i < 20; i=i+2){
+    console.log('odd number:', i)
+}
