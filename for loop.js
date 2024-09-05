58940ba35a018754c7980ab25ccb31ac5904bec7
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 14:56:06 +0600

    sum of numbers using for loop

--- for loop.js
+++ for loop.js
@@ -1,17 +1,25 @@
-// simple use of for loop
+// // simple use of for loop
 
-// for loop 
+// // for loop 
 
-for (let num = 1; num <= 5 ; num++){
-    console.log(num)
+// for (let num = 1; num <= 5 ; num++){
+//     console.log(num)
+// }
+
+// // even numbers print using for loop 
+// for (let i = 0; i<=20; i=i+2){
+//     console.log('even number:', i)
+// }
+
+// // odd numbers print using for loop 
+// for (let i = 1; i < 20; i=i+2){
+//     console.log('odd number:', i)
+// }
+
+// sum of numbers using for loop 
+let sum = 0;
+for (let i = 255; i <399 ; i++){
+    sum = sum + i;
+    console.log(i)
 }
-
-// even numbers print using for loop 
-for (let i = 0; i<=20; i=i+2){
-    console.log('even number:', i)
-}
-
-// odd numbers print using for loop 
-for (let i = 1; i < 20; i=i+2){
-    console.log('odd number:', i)
-}
+console.log('sum of numbers are :', sum)
